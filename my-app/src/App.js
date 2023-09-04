773f4dd6ec21085bffb6d5092409114493dc1d9c
Jione Jang <jenny.jione.jang@gmail.com>
2023-09-04 16:38:36 +0900

    feat: initial front-end code

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>나의 간단한 React 웹사이트</h1>
+      <p>환영합니다! 이것은 간단한 React 웹사이트입니다.</p>
+    </div>
+  );
+}
+
+export default App;
