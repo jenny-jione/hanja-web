7e1828e885359bcbfa421b977a0e26ab7df5316c
Jione Jang <jenny.jione.jang@gmail.com>
2023-09-05 16:45:46 +0900

    add: hanja, grade table 생성

--- api/server/models.py
+++ api/server/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, BigInteger, String, Date
+from sqlalchemy import Column, BigInteger, String, VARCHAR, Date
 
 class UserTable(Base):
     __tablename__ = "users"
@@ -8,4 +8,30 @@ class UserTable(Base):
     user_id = Column(String(255), unique=True, index=True)
     password = Column(String(255), nullable=False)
     user_name = Column(String(255), nullable=False)
-    birthday = Column(Date, nullable=True)  # 생년월일
+    birthday = Column(Date, nullable=True)
+
+
+class HanTable(Base):
+    __tablename__ = "han"
+    
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    hanja = Column(VARCHAR(10), unique=True, index=True)
+    kor = Column(VARCHAR(30), nullable=False)
+    radical = Column(VARCHAR(10), nullable=False)
+    radical_name = Column(VARCHAR(30), nullable=False)
+    stroke_count = Column(VARCHAR(10), nullable=False)
+    level = Column(VARCHAR(10), nullable=False)
+    rep_pron = Column(VARCHAR(10), nullable=False)
+
+# hanja, kor, radical, radical_name, stroke_count, level, rep_pron
+
+
+class GradeTable(Base):
+    __tablename__ = "grade"
+    
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    h_id = Column(BigInteger, nullable=False)
+    count = Column(BigInteger, nullable=False)
+    user_id = Column(String(255), nullable=False)
+    
+
