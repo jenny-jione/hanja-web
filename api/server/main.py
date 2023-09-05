58bbb1a20f1a971afdfd722a52f77bace2052f0c
Jione Jang <jenny.jione.jang@gmail.com>
2023-09-05 16:52:50 +0900

    feat: router 연결

--- api/server/main.py
+++ api/server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from sqlalchemy import event, DDL
-from server.router.check import router_check
+from server.router.han import router_han
+from server.router.ini import router_ini
 from .models import UserTable
 from .database import Base, engine
 
@@ -9,16 +10,17 @@ INSERT INTO users (user_id, password, user_name, birthday)
 VALUES ('jenny', '1234', '장지원', '1995-10-27')
 """
 event.listen(UserTable.__table__, 'after_create', DDL(init_insert_sql))
-Base.metadata.drop_all(bind=engine)
+# Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 def get_server():
     server = FastAPI(
-        title="happi",
+        title="h-api",
         docs_url="/docs",
         redoc_url=None
     )
-    server.include_router(router_check)
+    server.include_router(router_han)
+    server.include_router(router_ini)
     
     return server
 
