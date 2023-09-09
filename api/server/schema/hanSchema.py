8e257a34510529fe7c4ba25d08b2b4514f3c7ff5
jenny.jione.jang <jenny.jione.jang@gmail.com>
2023-09-09 22:14:28 +0900

    feat: schema 정의

---
+++ api/server/schema/hanSchema.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional
+from enum import Enum
+
+
+class SortOrder(str, Enum):
+    ganada = 'ganada'
+    count_asc = 'count_asc'
+    count_desc = 'count_desc'
+
+
+class HanListOut(BaseModel):
+    id: int
+    hanja: str
+    kor: str
+    radical: str
+    radical_name: str
+    level: str
+    count: Optional[int]
+
+    class Config:
+        orm_mode = True
