7f29500f9433f9c60f1d1db9b21da1e0c876d154
jenny.jione.jang <jenny.jione.jang@gmail.com>
2023-09-11 19:39:02 +0900

    modify: 401 에러핸들링 조건문 변경

--- my-app/src/js/api.js
+++ my-app/src/js/api.js
@@ -7,9 +7,7 @@ const catchError = async error => {
     const { detail } = error?.response?.data;
     switch (status) {
       case 401:
-        if (detail === 'Incorrect username or password')
           return alert('아이디 또는 비밀번호가 틀렸습니다.\n다시 입력해 주세요.');
-        break;
       case 500:
       case 504:
         return 'serverError';
