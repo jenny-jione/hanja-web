94c8ec05c03b8e9a299096b23d97193539175bd0
Jione Jang <jenny.jione.jang@gmail.com>
2023-09-11 15:15:27 +0900

    design: Navbar에 list 추가

--- my-app/src/component/RBSNav.js
+++ my-app/src/component/RBSNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 
 function CommonNavbar() {
@@ -8,6 +9,9 @@ function CommonNavbar() {
         <Navbar className="justify-content-center" bg="light" data-bs-theme="light">
             <Container>
                 <Navbar.Brand href="/">Home</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/list">list</Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
     );
